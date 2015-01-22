e491b62f6c3cf10be46a974bef14566551534ef1
Mat Sletten <matsletten@lyellenimac.bbbb.net>
2015-01-22 15:15:24 -0800

    updates

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-                            
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    var myLocation:CLLocationManager!
+    //can use var myLocation = CLLocationManager() in place of above
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLocation = CLLocationManager()
+        myLocation.delegate = self
+        myLocation.desiredAccuracy = kCLLocationAccuracyBest
+        myLocation.requestWhenInUseAuthorization()
+        myLocation.startUpdatingLocation()
+    }
+    
+    func locationManager(myLocation: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        speedLabel.text = "\(userLocation.speed)"
+        courseLabel.text = "\(userLocation.course)"
+        altitudeLabel.text = "\(userLocation.altitude)"
+        //Below we use a method to find the addresses of nearby places according to our location
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:{(placemarks, error) in
+            if error != nil {println(error)}
+            else {
+                println(placemarks)
+                let place = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                var addressNumber:String
+                if (place.subThoroughfare != nil) {
+                    addressNumber = place.subThoroughfare
+                }
+                else {
+                    addressNumber = ""
+                }
+                self.addressLabel.text = "\(place.subLocality)\n\(addressNumber) \(place.thoroughfare)\n\(place.subAdministrativeArea), \(place.administrativeArea)\n\(place.country)"
+            }
+        })
+        //println(userLocation)
     }
 
     override func didReceiveMemoryWarning() {
