8faaa80188544aa606bf544b54f8a54399c22936
Mat Sletten <matsletten@lyellenimac.bbbb.net>
2015-01-21 16:40:23 -0800

    Initial Commit

---
+++ Where Am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Am I
+//
+//  Created by Mat Sletten on 1/21/15.
+//  Copyright (c) 2015 Mat Sletten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
